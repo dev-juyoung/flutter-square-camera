f96702e2826754010c5f58bcc2d17d1007a9cbae
Juyoung, Lee <dev.juyoung@gmail.com>
2020-12-01 17:13:28 +0900

    [ADDED] iOS 런타임 권한 관련 로직 구현

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,39 @@ class CameraApp extends StatefulWidget {
 }
 
 class _CameraAppState extends State<CameraApp> {
+  bool _isAvailable = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _hasPermissions();
+  }
+
+  void _hasPermissions() async {
+    final result = await SquareCamera.hasPermissions();
+    print('[hasPermissions]::$result');
+
+    if (_isAvailable == result) {
+      return;
+    }
+
+    setState(() {
+      _isAvailable = result;
+    });
+  }
+
+  void _requestPermissions() async {
+    final result = await SquareCamera.requestPermissions();
+    print('[requestPermissions]::$result');
+
+    if (!result) {
+      return;
+    }
+
+    _hasPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +57,31 @@ class _CameraAppState extends State<CameraApp> {
         appBar: AppBar(
           title: Text('Square Camera Demo'),
         ),
-        body: Container(),
+        body: _isAvailable
+            ? Container()
+            : Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('사진 촬영 및 저장을 위해 권한을 획득하세요.'),
+              SizedBox(height: 8.0),
+              RaisedButton.icon(
+                icon: Icon(Icons.lock_open_rounded),
+                label: Text('권한 획득'),
+                onPressed: _requestPermissions,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: _isAvailable
+            ? FloatingActionButton(
+          child: Icon(Icons.camera_rounded),
+          onPressed: () async {},
+        )
+            : null,
       ),
     );
   }
--- ios/Classes/SwiftSquareCameraPlugin.swift
+++ ios/Classes/SwiftSquareCameraPlugin.swift
@@ -1,14 +1,45 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 public class SwiftSquareCameraPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "square_camera", binaryMessenger: registrar.messenger())
-    let instance = SwiftSquareCameraPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    static let TAG = "[Plugins] SquareCamera"
+    private static let METHOD_CHANNEL = "plugins.juyoung.dev/square_camera"
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftSquareCameraPlugin()
+        let channel = FlutterMethodChannel(name: METHOD_CHANNEL, binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "hasPermissions":
+            hasPermissions(call: call, result: result)
+        case "requestPermissions":
+            requestPermissions(call: call, result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func hasPermissions() -> Bool {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    private func hasPermissions(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(hasPermissions())
+    }
+    
+    private func requestPermissions(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            result(granted)
+        }
+    }
 }
+
